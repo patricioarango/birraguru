3a164c029442613d7e65d73cb86ff0e3047a1817
patricioarango <patricioarango@gmail.com>
2017-03-16 16:06:46 -0300

    season finale

--- www/js/controles.js
+++ www/js/controles.js
@@ -60,7 +60,10 @@ function show_card(card){
         '</div>');      
     } 
 
-
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 
 
 
